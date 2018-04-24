a6d536d7f365ba472a57086f302921003abb8a2b
sleeplesseditor <josh@bryson.me.uk>
2018-04-24 17:56:59 +0100

    AlbumDetail and Card components created

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
@@ -10,12 +12,18 @@ class AlbumList extends Component {
             .then(response => this.setState({ albums: response.data }));
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
     render() {
         console.log(this.state);
 
         return (
             <View>
-                <Text>Album List</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
