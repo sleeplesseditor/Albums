5bb201dc3e09667e3245148c25e8e22705b3390e
sleeplesseditor <josh@bryson.me.uk>
2018-04-25 11:50:34 +0100

    Finalisation of app

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ To get this up and running, follow these steps:
 iOS version (ensure you have xCode installed)
 1) cd albums
 2) react-native run-ios
+
+
+![screen shot 2018-04-25 at 11 37 39 2](https://user-images.githubusercontent.com/25869284/39240972-531fd8f0-487d-11e8-8e18-2ec707b7e7c3.png)
+App showing albums with linked button
+
+
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Linking, Text, View } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { thumbnailStyle, 
         headerContentStyle, 
         thumbnailContainerStyle,
@@ -27,11 +28,18 @@ const AlbumDetail = ({ album }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image 
                     style={imageStyle}
                     source={{ uri: image }} 
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
